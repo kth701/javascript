35fa9355475b7e84d6076b0d79697144c3e8db35
kthnaver <kth_701@naver.com>
2024-04-23 10:35:02 +0900

    - filter2

--- nodejs/js/arr_filter.js
+++ nodejs/js/arr_filter.js
@@ -1,4 +1,10 @@
 // 배열.filter(콜백함수)
 // 조건을 만족하는 데이터의배열 생성
-const newArr = [10,20,30,40].filter( (data)=> data>=30 )
+const num = [10,20,30,40]
+const newArr = num.filter( (data)=> data>=30 )
 console.log('30이상: ',newArr)
+
+const newArr2 = num.filter( function (data) { 
+  return data<=30
+})
+console.log('30이하:',newArr2)
