e4f873231240257864095a19d1401ed89d0b74f6
kthnaver <kth_701@naver.com>
2024-05-07 17:09:01 +0900

    - fetch

---
+++ nodejs/js_test/ex09_fetch.js
@@ -0,0 +1,48 @@
+
+// 테스트 형식 읽기
+async function load() {
+  const data = await fetch('sample.txt')
+  const text = await data.text()
+
+  console.log(text)
+  
+}
+//load() //호출
+
+// JSON형식 읽기
+async function json_load(){
+  const data = await fetch('sample.json')
+  const json_data = await data.json()
+
+  console.log(json_data)
+  console.log(JSON.stringify(json_data, null, '  '))
+
+}
+// json_load()
+
+// XML데이터 읽기
+async function json_load2(){
+  const data = await fetch('sample.xml')
+  const text = await data.text() // 텍스트 형식
+  // XML형식으로 해석
+  const xml = new DOMParser().parseFromString(text, 'application/xml')
+
+  console.log(text)
+  document.querySelector('#log').innerHTML = text
+
+}
+// json_load2()
+
+// XML데이터 읽기
+async function image_load(){
+  const data = await fetch('life.jpg')
+  const blob = await data.blob() // blob 형식
+
+  // img 요소 생성
+  const image = new Image()
+  image.src = URL.createObjectURL(blob)
+
+  document.querySelector('#log').appendChild(image)
+
+}
+image_load()
