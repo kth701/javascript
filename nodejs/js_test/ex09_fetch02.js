c2958eb47bcdb792c542e6f31faf2adccd8e10a6
kthnaver <kth_701@naver.com>
2024-05-09 12:50:00 +0900

    - REST API GET 요청

--- nodejs/js_test/ex09_fetch02.js
+++ nodejs/js_test/ex09_fetch02.js
@@ -13,19 +13,30 @@ const getData = async ()=>{
     const data = await resp.json() // json구조로 전환 //console.log(data)
     
     data.forEach(item => {
-      //console.log(item)
+      console.log(item)
       // for (변수 of 배열), for (키 in json객체)
 
       console.log("------")
       for (let key in item ){
+        //console.log(item.key)
+          
         
-        // value값이 object이면 처리
-        if (typeof item[key] === 'object'){
+        if (typeof item[key] === 'object'){ // value값이 object이면 처리
           console.log(`${key}값은 data가 아닌 객체입니다.`)
 
           let item2 = item[key] //console.log(key)
-          for (let key2 in item2) 
+          for (let key2 in item2){   // address객체에 대한 속성
             console.log(`${key}:${key2}=>${item2[key2]}`) 
+
+            if (typeof item2[key2] === 'object'){ // geo객체에 대한 속성
+              let item3 = item2[key2]
+              for (let key3 in item3)
+                console.log(`${key2}:${key3}=>${item3[key3]}`) 
+            }
+
+          }// end for
+
+
         } else { //  value가 데이터이면
           console.log(`${key}=>${item[key]}`) //  item.key == item[key]
         }
